4af06be3ed08c235ead6e85b55ee0f5e9ebf80e6
Christopher Mark Gore <cgore@cgore.com>
2021-03-18 23:40:49 -0500

    cluster-id

--- source/main.lisp
+++ source/main.lisp
@@ -3,6 +3,10 @@
   (:export "cluster-id"))
 (in-package #:ipfs-cluster)
 
+(defun cluster-id ()
+  "Cluster ID"
+  (gethash "id" (ipfs-cluster/rest-api:get-cluster-id)))
+
 (defun cluster-version ()
   "Cluster version"
   (gethash "version" (ipfs-cluster/rest-api:get-cluster-version)))
