83c2b907df28c17c501e8f1283d4ae79ded8c44e
Christopher Mark Gore <cgore@cgore.com>
2021-03-18 23:43:09 -0500

    rpc-protocol-version

--- source/main.lisp
+++ source/main.lisp
@@ -7,6 +7,14 @@
   "Cluster ID"
   (gethash "id" (ipfs-cluster/rest-api:get-cluster-id)))
 
+(defun rpc-protocol-version ()
+  "Cluster ID"
+  (gethash "rpc_protocol_version" (ipfs-cluster/rest-api:get-cluster-id)))
+
+(defun cluster-peername ()
+  "Cluster peer name"
+  (gethash "peername" (ipfs-cluster/rest-api:get-cluster-id)))
+
 (defun cluster-version ()
   "Cluster version"
   (gethash "version" (ipfs-cluster/rest-api:get-cluster-version)))
